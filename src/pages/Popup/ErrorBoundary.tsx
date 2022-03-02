5877335503b30f1f2eeb232e262beacf1ea22d19
JEFFe <jeffe@jeffe.co>
2022-03-02 04:44:49 +0200

    Eslint iloiseksi näillä jekuilla

--- src/pages/Popup/ErrorBoundary.tsx
+++ src/pages/Popup/ErrorBoundary.tsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { ErrorInfo } from 'react';
 import * as Sentry from '@sentry/react';
 import { Alert, Button } from '@mantine/core';
 import ErrorOutlined from '@mui/icons-material/ErrorOutlined';
 
 class ErrorBoundary extends React.Component {
-  constructor(props) {
+  constructor(props: any) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
-  componentDidCatch(error, errorInfo) {
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     // You can also log the error to an error reporting service
     console.log('ERRORIHA SE ON', error, errorInfo);
     Sentry.captureException(error)
   }
 
   render() {
-    if (this.state.hasError) {
+    const { state, props } = this
+    if ((state as {hasError: boolean}).hasError) {
       // You can render any custom fallback UI
       return (
-        <Alert icon={<ErrorOutlined size={16} />} title="Virhe" color="red">
+        <Alert icon={<ErrorOutlined />} title="Virhe" color="red">
           Joku meni todella pahasti rikki. Voit yrittää uudelleen tai luovuttaa.
           {' '}
           <br />
@@ -33,7 +34,7 @@ class ErrorBoundary extends React.Component {
       );
     }
 
-    return this.props.children;
+    return props.children;
   }
 }
 
