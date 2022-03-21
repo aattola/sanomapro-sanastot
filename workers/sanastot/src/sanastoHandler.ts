6ca6d34246852edb7fdf621ba7121379f11fc885
JEFFe <jeffe@jeffe.co>
2022-03-21 14:20:32 +0200

    On Track 1-8 sanastot

--- workers/sanastot/src/sanastoHandler.ts
+++ workers/sanastot/src/sanastoHandler.ts
@@ -1,11 +1,55 @@
 import { Request, RouteHandler } from 'itty-router';
 import ruotsi from './ruotsi.json'
+import ontrack from './on_track.json'
+
+const on_track = {
+  "state": "published",
+  "hidden": false,
+  "version": "1.0",
+  "primaryColor": "#cc6699",
+  "language": "en",
+  "bundleId": "custom_on_track",
+  "bundleTitle": "On Track",
+  "materialTitle": "On Track",
+  "materialId": "custom_on_track",
+  "productId": "JEFF-2",
+  "updatedAt": "2022-02-22T08:15:54.421Z",
+  "createdAt": "2022-02-22T14:41:42.562Z",
+  "coverImage": "https://i.imgur.com/mIS3hlU.png",
+  "coverImages": {
+    "large": {
+      "height": 1410,
+      "width": 1092,
+      "url": "https://i.imgur.com/KYtr2H0.png"
+    },
+    "medium": {
+      "height": 705,
+      "width": 546,
+      "url": "https://i.imgur.com/KYtr2H0.png"
+    },
+    "small": {
+      "height": 353,
+      "width": 274,
+      "url": "https://i.imgur.com/KYtr2H0.png"
+    }
+  },
+  alphaEntries: ontrack
+}
 
 const sanastoHandler: RouteHandler<Request> = async (req) => {
   const {params} = req
 
   if (!params || !params.id) {
     throw new Error("Paramit puuttuu (id)")
+  }
+
+  if (params.id === "custom_on_track") {
+    return new Response(JSON.stringify(on_track), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+    })
   }
 
   if (params.id === "custom_ruotsi") {
--- workers/sanastot/src/sanastotHandler.ts
+++ workers/sanastot/src/sanastotHandler.ts
@@ -42,6 +42,38 @@ const sanastotHandler: RouteHandler<Request> = async (req) => {
         }
       }
     },
+    {
+      "state": "published",
+      "hidden": false,
+      "version": "1.0",
+      "primaryColor": "#cc6699",
+      "language": "en",
+      "bundleId": "custom_on_track",
+      "bundleTitle": "On Track",
+      "materialTitle": "On Track",
+      "materialId": "custom_on_track",
+      "productId": "JEFF-2",
+      "updatedAt": "2022-02-22T08:15:54.421Z",
+      "createdAt": "2022-02-22T14:41:42.562Z",
+      "coverImage": "https://i.imgur.com/mIS3hlU.png",
+      "coverImages": {
+        "large": {
+          "height": 1410,
+          "width": 1092,
+          "url": "https://i.imgur.com/KYtr2H0.png"
+        },
+        "medium": {
+          "height": 705,
+          "width": 546,
+          "url": "https://i.imgur.com/KYtr2H0.png"
+        },
+        "small": {
+          "height": 353,
+          "width": 274,
+          "url": "https://i.imgur.com/KYtr2H0.png"
+        }
+      },
+    },
     ...data
   ]
   return new Response(JSON.stringify(returnData), {
